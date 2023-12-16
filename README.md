5fd0342a73897e17ed8bf1c70ff768bdc5910095
ReptilianEye <piotr.rzadkowski@outlook.com>
2023-12-16 23:43:59 +0100

    add day16 code

--- README.md
+++ README.md
@@ -26,7 +26,7 @@
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
-![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
@@ -167,16 +167,16 @@ Both parts: 580.508ms
 
 ```
 Day 15
-Time part 1: 4.59ms
-Time part 2: 16.747ms
-Both parts: 21.337ms
+Time part 1: 5.272ms
+Time part 2: 18.741ms
+Both parts: 24.013ms
 ```
 
 ```
 Day 16
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 76.138ms
+Time part 2: 2148.72ms
+Both parts: 2224.857ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 30/50
-Total time: 2147.204ms
+Total stars: 32/50
+Total time: 4374.738ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day16/index.ts
@@ -0,0 +1,181 @@
+import run from "aocrunner"
+const makeArrayOfSets = (n: number, m: number) => {
+  var arr = [] as Set<String>[][]
+  for (let i = 0; i < n; i++) {
+    arr[i] = []
+    for (let j = 0; j < m; j++) {
+      arr[i][j] = new Set()
+    }
+  }
+  return arr
+}
+type MapElement = "." | "-" | "|" | "/" | "J"
+const moves = {
+  ".": [],
+  "|": [
+    [-1, 0],
+    [1, 0],
+  ],
+  "-": [
+    [0, -1],
+    [0, 1],
+  ],
+  "/": [
+    [
+      [0, -1],
+      [-1, 0],
+    ],
+    [
+      [0, 1],
+      [1, 0],
+    ],
+  ],
+  J: [
+    [
+      [0, -1],
+      [1, 0],
+    ],
+    [
+      [0, 1],
+      [-1, 0],
+    ],
+  ],
+}
+export const move = (start: number[], moveCoord: number[]) => [
+  start[0] + moveCoord[0],
+  start[1] + moveCoord[1],
+]
+const validPos = (map: any[][], pos: number[]) => {
+  const n = map.length
+  const m = map[0].length
+  if (pos[0] < 0 || pos[0] >= n) return false
+  if (pos[1] < 0 || pos[1] >= m) return false
+  return true
+}
+const substract = (a: number[], b: number[]) => [a[0] - b[0], a[1] - b[1]]
+const getNbours = (map: MapElement[][], pos: number[], prev: number[]) => {
+  let el = map[pos[0]][pos[1]]
+  let prevUnitVec = substract(prev, pos)
+  switch (el) {
+    case ".":
+      return [substract(pos, prev)]
+    case "/":
+    case "J": {
+      let nb = moves[el]
+      for (let n of nb) {
+        let i = n.findIndex((v) => v.toString() === prevUnitVec.toString())
+        if (i !== -1) return [n[1 - i]]
+      }
+      throw new Error("Invalid map")
+    }
+    case "|":
+    case "-": {
+      let nb = moves[el]
+      let i = nb.findIndex((n) => n.toString() === prevUnitVec.toString())
+      if (i !== -1) return [nb[1 - i]]
+      return nb
+    }
+  }
+}
+const dfs = (map: string[][], start: number[], startPrev: number[]) => {
+  const dfs_rec = (pos: number[], prev: number[]) => {
+    if (vis[pos[0]][pos[1]].has(prev.toString())) return
+    vis[pos[0]][pos[1]].add(prev.toString())
+    for (let nb of getNbours(map as MapElement[][], pos, prev)) {
+      let newPos = move(pos, nb)
+      if (validPos(map, newPos)) dfs_rec(newPos, pos)
+    }
+  }
+  const vis = makeArrayOfSets(map.length, map[0].length)
+  dfs_rec(start, startPrev)
+  return vis.reduce(
+    (acc, line) =>
+      acc + line.reduce((accL, s) => (s.size > 0 ? accL + 1 : accL), 0),
+    0,
+  )
+}
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((l) => l.split(""))
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  return dfs(input, [0, 0], [0, -1])
+}
+enum Side {
+  Top,
+  Right,
+  Bottom,
+  Left,
+}
+const getSideData = (
+  map: string[][],
+  side: Side,
+): { start: number[]; startPrev: number[]; limit: number } => {
+  const n = map.length
+  const m = map[0].length
+  switch (side) {
+    case Side.Top:
+      return { start: [0, NaN], startPrev: [-1, NaN], limit: m }
+    case Side.Right:
+      return { start: [NaN, 0], startPrev: [NaN, -1], limit: n }
+    case Side.Bottom:
+      return { start: [n - 1, NaN], startPrev: [n, NaN], limit: m }
+    case Side.Left:
+      return { start: [NaN, m - 1], startPrev: [NaN, m], limit: n }
+  }
+}
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  const sides = Object.values(Side).slice(4) as Side[]
+  var max = 0
+  for (let side of sides) {
+    const { start, startPrev, limit } = getSideData(input, side)
+    for (let i = 0; i < limit; i++) {
+      let startDFS = start.map((v) => (isNaN(v) ? i : v)) as number[]
+      let startPrevDFS = startPrev.map((v) => (isNaN(v) ? i : v)) as number[]
+      max = Math.max(max, dfs(input, startDFS, startPrevDFS))
+    }
+  }
+  return max
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `.|...J....
+|.-.J.....
+.....|-...
+........|.
+..........
+.........J
+..../.JJ..
+.-.-/..|..
+.|....-|.J
+..//.|....`,
+        expected: 46,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `.|...J....
+|.-.J.....
+.....|-...
+........|.
+..........
+.........J
+..../.JJ..
+.-.-/..|..
+.|....-|.J
+..//.|....`,
+        expected: 51,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  // onlyTests: true,
+})
